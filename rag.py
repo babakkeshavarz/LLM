a4f284fdfb460fdc88926318a9221ea60c5ad526
babak_keshavarz <babak0keshavarz@gmail.com>
2025-09-13 08:38:43 -0700

    added adam's biography

--- rag.py
+++ rag.py
@@ -2,7 +2,7 @@ import ollama
 
 # Load the dataset
 dataset = []
-with open("cat-facts.txt", "r", encoding="utf-8") as file:
+with open("adam-smith-a-biography.txt", "r", encoding="utf-8") as file:
     dataset = file.readlines()
     print(f'Loaded {len(dataset)} entries')
 
